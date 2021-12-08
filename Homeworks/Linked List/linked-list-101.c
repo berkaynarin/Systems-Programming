0c01212ec82f66b06e104f6922097a7bad9a0350
Berkay Narin <bnarin.dev@gmail.com>
2021-12-08 16:49:49 +0300

    Fix the bug that causes infinite loop

--- Homeworks/Linked List/linked-list-101.c
+++ Homeworks/Linked List/linked-list-101.c
@@ -1,62 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h> //for memory allocation operations
 
 void main() {
 
 	struct Node {
 		int value; //value of the node
-		struct Node* next; //pointer of this struct
+		struct Node *next; //pointer of this struct
 	};
 	
-	struct Node* head = NULL; //points to head of the list
-	struct Node* tail = NULL; //points to tail of the list
-	struct Node* first = NULL;
-	struct Node* second = NULL;
-	struct Node* third = NULL; //create nodes 
+	struct Node *head = NULL; //points to head of the list
+	struct Node *tail = NULL; //points to tail of the list		
 	
-	
-	//Allacote memory for nodes
-	
-	head = (struct Node*)malloc(sizeof(struct Node)); // allacote memory as much as size of the struct
-	tail = (struct Node*)malloc(sizeof(struct Node));
-	/*first = (struct Node*)malloc(sizeof(struct Node));  
-	second = (struct Node*)malloc(sizeof(struct Node));
-	third = (struct Node*)malloc(sizeof(struct Node));	
-	
-	head->next = first;
-	tail->next = first;
-	
-	first->value = 1;
-	first->next = second;*/
-	
-	/*second->value = 5;
-	second->next = third;
-	
-	third->value = 8;
-	third->next = NULL;*/
-	
-	void insertHead(struct Node* head, int newValue) {
-		struct Node* newNode = NULL;
+	void insertHead(struct Node* Head, int newValue) {
+		struct Node *newNode = NULL;
 		newNode = (struct Node*)malloc(sizeof(struct Node));
 		newNode->value = newValue; //Set newNode's value to given value
-		newNode->next = head->next;	//Set newNode's pointer's to old head
-		head->next = newNode;		
+		if(Head == NULL) { //It controls the parameter Head to see if the list is empty 
+			head = (struct Node*)malloc(sizeof(struct Node));  //It allocates for actual head
+			tail = (struct Node*)malloc(sizeof(struct Node)); //We should allacote memory inside the if, if we do it outside, we cannot check if it's null or not
+			tail->next = newNode; //Should also set the tail because tail points to null in the first place 
+			newNode->next = NULL; //First node's next pointer points to null
+			head->next = newNode;
+		}
+		else {
+			newNode->next = head->next;	//Set newNode's pointer's to old head		
+			head->next = newNode;	
+		}
+							
 	}
 
-	void insertTail(struct Node* tail, int newValue){
-		struct Node* newNode = NULL;
+	void insertTail(struct Node* Tail, int newValue){
+		struct Node *newNode = NULL;
 		newNode = (struct Node*)malloc(sizeof(struct Node));
 		newNode->value = newValue;
+		if(Tail == NULL) { //It controls the parameter Tail to see if the list is empty
+			head = (struct Node*)malloc(sizeof(struct Node));  //It allocates for actual head
+			tail = (struct Node*)malloc(sizeof(struct Node)); 
+			tail->next = newNode; //Should also set the tail because tail points to null in the first place 
+			head->next = newNode;
+		}
+		else {
+			Tail->next->next = newNode; //Set old tail's next pointer to new one
+			Tail->next = newNode; //Set the tail pointer of the list to new one
+		}	
 		newNode->next = NULL; //Set next pointer to null because it'll be new the tail
-		tail->next->next = newNode; //Set old tail's next pointer to new one
-		tail->next = newNode; //Set the tail pointer of the list to new one
 	}	
 	
 	insertHead(head, 5);
-	insertTail(tail, 14);
-	insertTail(tail, 17);
+	insertHead(head, 7);
+	insertHead(head, 13);
+	insertTail(tail, 14);	
 	
-	
-	struct Node* i = head;
+	struct Node *i = head;
 	int j = 1;
 	
 	while(i != NULL) {
