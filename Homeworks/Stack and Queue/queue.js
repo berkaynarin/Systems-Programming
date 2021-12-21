706f186b13b8426a83db273156ee57bf1e0be265
Berkay Narin <bnarin.dev@gmail.com>
2021-12-21 21:44:43 +0300

    Add a simple user-interface to get input from user

--- Homeworks/Stack and Queue/queue.js
+++ Homeworks/Stack and Queue/queue.js
@@ -2,53 +2,60 @@ let head = 0; // Identifies the index of start of the queue
 let tail = 0; // Identifies the index of end of the queue
 let numArray = [];
 let maxValue = 5; //Maximum size of the queue
+const prompt = require("prompt-sync")(); //To read input from console
 
 function enqueue(value) {
-    if(tail + 1 > maxValue) { //Check if queue is full
-        console.log("Cannot enqueue a new element. The queue is full.")
-    }
-    else {
-        numArray[tail] = value; // Insert a new element to the end of the queue
-        tail++;
-    }
+  if (tail + 1 > maxValue) {
+    //Check if queue is full
+    console.log("Cannot enqueue a new element. The queue is full.");
+  } else {
+    numArray[tail] = value; // Insert a new element to the end of the queue
+    tail++;
+  }
 }
 
 function dequeue() {
-    if(tail < 1) { // Check if queue is empty
-        console.log("Cannot dequeue an element. The queue is empty.")
+  if (tail < 1) {
+    // Check if queue is empty
+    console.log("Cannot dequeue an element. The queue is empty.");
+  } else {
+    for (let i = head; i < tail; i++) {
+      numArray[i] = numArray[i + 1]; //Shifting elements up starting from top
     }
-    else {
-        for (let i = head; i < tail; i++) {
-            numArray[i] = numArray[i+1]; //Shifting elements up starting from top
-        }
-        numArray[tail - 1] = null; //Deleting last element from the queue
-        tail--;
-    }
+    numArray[tail - 1] = null; //Deleting last element from the queue
+    tail--;
+  }
 }
 
 function display() {
-    if(tail < 1) { // Check if queue is empty
-        console.log("Cannot display the queue. The queue is empty.")
+  if (tail < 1) {
+    // Check if queue is empty
+    console.log("Cannot display the queue. The queue is empty.");
+  } else {
+    for (let i = head; i < tail; i++) {
+      console.log(i + 1 + ". element: " + numArray[i]);
     }
-    else {
-        for (let i = head; i < tail; i++) {
-            console.log((i+1) + ". element: " + numArray[i]);
-        }
-    }
+  }
 }
 
-enqueue(4);
-enqueue(8);
-enqueue(7);
-enqueue(63);
-enqueue(89);
-dequeue();
-enqueue(21);
-enqueue(56); // Should show an error for this one 
-display(); // Should display 8,7,63,89,21
+console.log("Queue implementation with JavaScript\n");
 
-
-
-
-
-
+do {
+  console.log(
+    "\n 1.Enqueue a new element \n 2.Dequeue an element \n 3.Display the queue \n 4.Exit the program\n\n"
+  );
+  var input = prompt("Please select one of the options below: ");
+  switch (Number(input)) {
+    case 1:
+      let value = prompt("Please enter the value you want to enqueue: ");
+      enqueue(Number(value)); //I cannot check here if the user is giving different types of elements
+      //because prompt method always stores input as strings
+      break;
+    case 2:
+      dequeue();
+      break;
+    case 3:
+      display();
+      break;
+  }
+} while (Number(input) != 4);
